b53ce51ec7af8c4470e123d663dee57fcd131a51
Arturo Clavero <arturoclavero01@gmail.com>
2025-07-28 00:40:25 +0700

    refactor: EthereumRouter safer integration

--- src/EthereumRouter.sol
+++ src/EthereumRouter.sol
@@ -1,24 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./interfaces/IL1GatewayRouter.sol";
 
 //deployed on ethereum!
-contract EthereumRouter {
+contract EthereumRouter is ReentrancyGuard{
+	using SafeERC20 for IERC20;
+
     error tokenNotMapped();
+	error DoubleOrder();
 
     event BridgeStarted(uint256 indexed orderId);
     event BridgeFinished(uint256 indexed orderId);
+
+	uint256 constant _stateFinished = 0;
+	uint256 constant _stateVerified = 1;
+	uint256 constant _statePending = 2;
+	uint256 constant _stateCancelled = 3;
+
+	mapping(uint256=>uint256) private orders;
 
     function verifyOrder() private pure returns (bool) {
         //TODO
         return true;
     }
 
+	function updateOrder(uint256 orderId, uint256 state) private{
+		orders[orderId] = state;
+	}
     //dst token address the address recipient ? To revise
-    function bridgeStart(uint256 amount, address srcTokenAddress, uint256 orderId) external payable {
-        address l1GatewayRouterAddress = 0xF8B1378579659D8F7EE5f3C929c2f3E332E41Fd6;
+    function bridgeStart(uint256 amount, address srcTokenAddress, uint256 orderId) external payable nonReentrant{
+
+		address l1GatewayRouterAddress = 0xF8B1378579659D8F7EE5f3C929c2f3E332E41Fd6;
+
+		if (orders[orderId] != 0)
+			revert DoubleOrder();
 
         //receive tokens
         IERC20(srcTokenAddress).transferFrom(msg.sender, address(this), amount);
@@ -43,5 +61,7 @@ contract EthereumRouter {
 
         //send event notfication
         emit BridgeStarted(orderId);
+
+		updateOrder(orderId, _statePending);
     }
 }
