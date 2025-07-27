9e1f643c476d1962971074575ee0532f8ebc5bc8
Arturo Clavero <arturoclavero01@gmail.com>
2025-07-27 20:08:21 +0700

    feat: router contract bridge

--- src/EthereumRouter.sol
+++ src/EthereumRouter.sol
@@ -1,38 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/IL1GatewayRouter.sol";
+
 //deployed on ethereum!
 contract EthereumRouter {
-	error invalidOrder();
+	error tokenNotMapped();
 	event BridgeStarted(uint256 indexed orderId);
-	event routeFinished(uint256 indexed orderId);
-
-
-	function routeStart(uint256 amount, uint256 orderId) external {
-		if (!verifyOrder())
-			revert invalidOrder();
-		swap("ETH", "USDC", amount);
-		bridge();
-		emit BridgeStarted(orderId);
-	}
-
-	function routeFinish(uint256 amount, uint256 orderId) external {
-		swap("USDC", "ETH", amount);
-		emit routeFinished(orderId);
-	}
-	
-	function swap(string memory from, string memory to, uint256 amount) private {
-		//TODO
-		//use 1 inch
-	}
-	function bridge() private {
-		//TODO
-		//use scroll bridge (ethereum to scroll)
-	}
+	event BridgeFinished(uint256 indexed orderId);
 
 	function verifyOrder() private pure returns (bool){
 		//TODO
 		return true;
 	}
+
+		//dst token address the address recipient ? To revise
+	function bridgeStart(uint256 amount, address srcTokenAddress, uint256 orderId) external payable{
+		address l1GatewayRouterAddress = 0xF8B1378579659D8F7EE5f3C929c2f3E332E41Fd6;
+		
+		//receive tokens
+		IERC20(srcTokenAddress).transferFrom(msg.sender, address(this), amount);
+
+		//approve token transfer 
+		IERC20(srcTokenAddress).approve(l1GatewayRouterAddress, amount);
+		
+		//get L2 token address
+		address dstTokenAddress = IL1GatewayRouter(l1GatewayRouterAddress).getL2ERC20Address(srcTokenAddress);
+    	if (dstTokenAddress == address(0))
+			revert tokenNotMapped();
+
+		//send ERC20
+		IL1GatewayRouter(l1GatewayRouterAddress).depositERC20 {value: msg.value } (
+			srcTokenAddress,
+			dstTokenAddress,
+			amount,
+			200_000,            
+			"0x"// Optional calldata
+		);
+
+		//send event notfication
+		emit BridgeStarted(orderId);
+	}
+
 }
 
--- src/ScrollRouter.sol
+++ src/ScrollRouter.sol
@@ -1,38 +1,46 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-//deployed on ethereum!
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/IL2GatewayRouter.sol";
+
+//deployed on scroll!
 contract ScrollRouter {
-	error invalidOrder();
+	error tokenNotMapped();
 	event BridgeStarted(uint256 indexed orderId);
-	event routeFinished(uint256 indexed orderId);
-
-
-	function routeStart(uint256 amount, uint256 orderId) external {
-		if (!verifyOrder())
-			revert invalidOrder();
-		swap("SCR", "USDC", amount);
-		bridge();
-		emit BridgeStarted(orderId);
-	}
-
-	function routeFinish(uint256 amount, uint256 orderId) external {
-		swap("USDC", "SCR", amount);
-		emit routeFinished(orderId);
-	}
-	
-	function swap(string memory from, string memory to, uint256 amount) private {
-		//TODO
-		//use Uniswap, SushiSwap, or a Scroll-specific DEX
-	}
-	function bridge() private {
-		//TODO
-		//use scroll bridge (scroll to ethereum)
-	}
+	event BridgeFinished(uint256 indexed orderId);
 
 	function verifyOrder() private pure returns (bool){
 		//TODO
 		return true;
 	}
-}
 
+			//dst token address the address recipient ? To revise
+	function bridgeStart(uint256 amount, address srcTokenAddress, uint256 orderId) external payable{
+		address l2GatewayRouterAddress = 0x4C0926FF5252A435FD19e10ED15e5a249Ba19d79;
+
+		//receive tokens
+		IERC20(srcTokenAddress).transferFrom(msg.sender, address(this), amount);
+
+		//approve token transfer 
+		IERC20(srcTokenAddress).approve(l2GatewayRouterAddress, amount);
+		
+		//get L2 token address
+		address dstTokenAddress = IL2GatewayRouter(l2GatewayRouterAddress).getL1ERC20Address(srcTokenAddress);
+    	if (dstTokenAddress == address(0))
+			revert tokenNotMapped();
+
+		//send ERC20
+		IL2GatewayRouter(l2GatewayRouterAddress).withdrawERC20 {value: msg.value } (
+			srcTokenAddress,
+			dstTokenAddress,
+			amount,
+			200_000,            
+			"0x"// Optional calldata
+		);
+
+		//send event notfication
+		emit BridgeStarted(orderId);
+	}
+
+}
