2c62968acfdccaf1654e673096a241865e6ec622
Valeria <70722334+Hikabu@users.noreply.github.com>
2025-08-03 07:58:37 +0700

    fix: deleted logs

--- src/components/SwapForm.jsx
+++ src/components/SwapForm.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState, useRef } from 'react';
 import { fetchQuote } from '../hooks/useQuoteFetcher';
-
-
 import HTLC_abi from "../../abi/HTLC.json";
 import { ethers, Contract } from "ethers";
 import axios from "axios";
@@ -11,13 +9,14 @@ const ethHtlc = import.meta.env.VITE_ETH_CONTRACT_ADDRESS;
 const scrollHtlc = import.meta.env.VITE_SCROLL_CONTRACT_ADDRESS;
 const relayer_address = import.meta.env.VITE_RELAYER_ADDRESS;
 console.log("relayer", relayer_address);
-
 const SwapForm = ({
   connected, walletAddress, oldCurrency, newCurrency,
   amount, setAmount, convertedPrice, setConvertedPrice
 }) => {
   const [isLoading, setIsLoading] = useState(false);
   
+//------ Price use effect ------//
+
   useEffect(() => {
     if (connected && amount && Number(amount) > 0) {
       setIsLoading(true);
@@ -37,19 +36,23 @@ const SwapForm = ({
     }
     
   }, [connected, amount, oldCurrency, newCurrency, walletAddress]);
-  
+
+
+//------ States ------//
   const [isSwapping, setIsSwapping] = useState(false); 
+  const [isLocking, setIsLocking] = useState(false); 
   const [isRefundable, setIsRefundable] = useState(false);
   const [timeLeft, setTimeLeft] = useState(false);
   const htlcRef = useRef(null);
   
   
+//------------------------------------------Swaping------------------------------------------//
   //the lock triggers next event - relayer listens for the event - get swapId hashlock->
   //-> triggers the event for relayer/resolver "lock the same amount on the scroll"
-  
-  
+
+  //------ Lock funds user side ------//
   const handleSwap = async () => {
-    if (isSwapping) return ;
+    if (isSwapping || isLocking) return ;
     try {
       setIsSwapping(true);
       const {hash, secret } = generateSecret();
@@ -68,26 +71,22 @@ const SwapForm = ({
         }
       htlcRef.current = new ethers.Contract(contract, HTLC_abi, signer);
       const htlcContract = htlcRef.current;
-      console.log("htlc contract via ref", htlcContract);
       const amount = ethers.parseEther("0.00000001");
-      const timelock =  Math.floor(Date.now() / 1000) + 60 * 5;
+      const timelock =  60; //sec
       const txn = await htlcContract.createSwap(
           walletAddress,
           bytesHashlock, 
           timelock,
           { value: amount }
       );
-      console.log("Tx sent:", txn.hash);
       await txn.wait();
       const response = await axios.get(`http://localhost:3000/api/swap/${hash}`);
       const swapId = response.data.swapId;
       localStorage.setItem("swapId", swapId);
-      console.log("swapId is and now in the storage ✝️", swapId);
       let tryes = 0;
       const maxTryes = 10;
       while (tryes < maxTryes){
         const response = await axios.get(`http://localhost:3000/api/ready/${hash}`);
-        console.log("response is:", response);
         if (response.data.ready){
           await withdraw();
           break;
@@ -107,8 +106,12 @@ const SwapForm = ({
     }
     finally{
       setIsSwapping(false);
+      setIsLocking(false);
     }
   }
+
+
+    //------ Withdraw user  ------//
   const withdraw = async () => {
     try {
         const provider = new ethers.BrowserProvider(window.ethereum);
@@ -116,25 +119,24 @@ const SwapForm = ({
         //define htlc
         const  contract = oldCurrency.toLowerCase() === "eth" ? ethHtlc : scrollHtlc;
           const htlcContract = new ethers.Contract(contract, HTLC_abi, signer);
-          console.log("htlc contract normal", htlcContract);
           //hetting secret hash swapid
           const hash = localStorage.getItem('hash') || '';
           const swapId = localStorage.getItem('swapId');
-          console.log("from the local✝️✝️", swapId);
           const secret = localStorage.getItem('secret') || '';
-          console.log("the secret is", secret);
           const txn = await htlcContract.withdraw(swapId, secret);
           await txn.wait();
           alert ("Withdraw successful");
         } catch (err){
-          console.error("Withdraw unseccessfull", err);
-          alert ("Withdraw failed: " + err.message);
+            console.error("Withdraw unseccessfull", err);
+            if (err.code !== 'CALL_EXCEPTION')
+              alert ("Withdraw failed: " + err.message);
         }
   };
-  useEffect(() => {
+  //------ Refund check and cleanong use effect ------//
+
+ useEffect(() => {
     const preRefundCheck = async () => {
       const swapId = localStorage.getItem('swapId');
-      console.log("in the use effect usong teh local storage", swapId);
       if (!walletAddress || !swapId)
           return;
       //in case refresh page happened
@@ -146,11 +148,15 @@ const SwapForm = ({
       htlcRef.current = htlcContract;
       const swapInfo = await htlcContract.getSwap(swapId);
       const curTime = Math.floor(Date.now() / 1000);
+
+      const timelockRow = Number(swapInfo.timelock);
+      const timelock = timelockRow > 1e10 ? Math.floor(timelockRow / 1000) : timelockRow;
   
       const isSender = swapInfo.sender.toLowerCase() === walletAddress.toLowerCase();
-      const refundable = isSender && !swapInfo.withdrawn && !swapInfo.refunded && curTime > Number(swapInfo.timelock);
-      const timeL = Number(swapInfo.timelock)- curTime;
-      setTimeLeft(timeL > 0 ? timeL : 0);
+      const refundable = isSender && !swapInfo.withdrawn && !swapInfo.refunded && curTime > timelock;
+
+      const timeLeft = timelock > curTime ? timelock - curTime : 0;
+      setTimeLeft(timeLeft);
       setIsRefundable(refundable);
 
       if (swapInfo.withdrawn || swapInfo.refunded){
@@ -162,31 +168,42 @@ const SwapForm = ({
         const swapId = localStorage.getItem('swapId');
         if (swapId || hash || secret){
           console.log("program end, cleaning the data");
-          localStorage.removeItem('secret', secret);
-          localStorage.removeItem('hash', hash);
-          localStorage.removeItem('swapId', swapId);
-
+          localStorage.removeItem('secret');
+          localStorage.removeItem('hash');
+          localStorage.removeItem('swapId');
         }
       }
-
     };  
     preRefundCheck();
-    const interval = setInterval(preRefundCheck, 10000);
+    const interval = setInterval(preRefundCheck, 1000);
     return () => clearInterval(interval);
   }, [walletAddress, oldCurrency]);
+
+   //------ Refund  ------//
 
   async function refund() {
     try {
         const swapId = localStorage.getItem('swapId');
-        console.log("swapId✝️ in loc", swapId);
         const htlcContract = htlcRef.current;
         const txn = await htlcContract.refund(swapId);
         await txn.wait();
-        alert ("refund successfully");
+
+        const result = await txn.wait();
+        if (result.status === 1){
+          alert ("refund successful");
+        } else {
+          alert("refund failed");
+        }
         
     } catch (error){
       console.error("failed can not refund", error.message);
-      alert ("failed to refund");
+      if (error.code === 4001){
+        alert("transaction rejected by user");
+      } else if (error.code === 'CALL_EXCEPTION'){
+        alert("Refund failed - contract error");
+      } else {
+        alert ("failed to refund");
+      }
     }
   }
 
@@ -214,8 +231,8 @@ const SwapForm = ({
 		)}
 
 
-      <button onClick={handleSwap} disabled={!amount || Number(amount) <= 0}>
-        Swap
+      <button onClick={handleSwap} disabled={!amount || Number(amount) <= 0 || isLocking || isSwapping}>
+        {isLocking ? "Locking..." : isSwapping ? "Processing..." : "Swap"}
       </button>
 
       {isRefundable && (
